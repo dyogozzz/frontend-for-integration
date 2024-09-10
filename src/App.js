8ece92b45e18bdf248cccad73a8818fa53dd7505
dyogozzz <dyogozanetti@hotmail.com>
2024-09-10 17:13:51 -0300

    integration br24 test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { useEffect } from 'react';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        const queryParams = new URLSearchParams(window.location.search);
+        const domain = queryParams.get('DOMAIN');
+        const protocol = queryParams.get('PROTOCOL');
+        const lang = queryParams.get('LANG');
+        const appSid = queryParams.get('APP_SID');
+
+        console.log(`Domain: ${domain}, Protocol: ${protocol}, Language: ${lang}, App SID: ${appSid}`);
+        // Agora você pode usar esses parâmetros no seu app
+    }, []);
+
+    return (
+        <div>
+            <h1>App Integrado com BR24</h1>
+        </div>
+    );
 }
 
 export default App;
